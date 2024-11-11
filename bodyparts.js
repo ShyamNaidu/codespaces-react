b4068de6c2a296d93701ad70e659e92ffa90a8e9
ShyamNaidu <syam.svs@gmail.com>
2024-11-11 10:50:23 +0000

    Updated content

---
+++ bodyparts.js
@@ -0,0 +1,88 @@
+const bodyparts1 = {
+	"CT Scan": [
+		'3D Reconsturction',
+		'Abdomen',
+		'Abdomen & Pelvis',
+		'Brain',
+		'Cardiac Score/Calcium Screening',
+		'Cervical Spine','Chest',
+		'Chest-PE Protocol (Note:See CTA)',
+		'Lower Extremity',
+		'Lumbar Spine',
+		'Maxillofacial',
+		'Orbital Face&Neck',
+		'Pelvis',
+		'Pevis',
+		'Soft Tissue Neck',
+		'Thoracic Spine',
+		'Upper Extremity'
+	
+	/* Add any new parameters here before ]; */
+	],
+
+	"MRI": [
+		'Orbital Face&Neck',
+		'TMJ','Shoulder, Elbow or Wrist(Upper Extremity, Joint)',
+		'Humerus, Forearm or Hand(Upper Extremity, Non-Joint)',
+		'Hip, Knee, or Ankle, (Lower Extremity, Joint)',
+		'Thigh, Lower Leg or Foot(Lower Extremity, Non-Joint)',
+		'Brain',
+		'Cervical Spine',
+		'Chest(Clavicle)',
+		'Breast',
+		'Thoracic Spine',
+		'Abdomen',
+		'Lumbar Spine',
+		'Pelvis',
+		'ABD / PEL / LOWER EXTREMITY w/ & w/o contrast with 72198 and 73725',
+		'MRA ANGIO LOWER EXTREMITY',
+		'MRA ABDOMEN',
+		'ABDOMEN w/ contrast',
+		'ABDOMEN w/ & w/o contrast',
+		'BRAIN w/o contrast',
+		'BRAIN w/ contrast',
+		'BRAIN w/ & w/o contrast',
+		'BRAIN SPECTROSCOPY',
+		'BREAST BILATERAL without contrast (IMPLANTS)',
+		'BREAST (Left or Right) w/o contrast',
+		'BREAST (Left or Right) w/ & w/o contrast',
+		'BREAST LOCALIZATION',
+		'BREAST w/ & w/o contrast',
+		'CERV SPINE w/o contrast',
+		'CERV SPINE w/ contrast',
+		'CERV SPINE w/ & w/o contrast',
+		'CHEST w/o contrast',
+		'CHEST w/ contrast',
+		'CHEST w/ & w/o contrast',
+		'CHEST','ELASTOGRAPHY',
+		'ENTEROGRAPHY w/ & w/o contrast',
+		'MRA HEAD w/o contrast',
+		'MRA HEAD w/ contrast',
+		'MRA HEAD w/ & w/o contrast',
+		'JOINT LOW EXTREMITY w/ & w/o contrast'
+	],
+	
+	"MRA-MRV(Angiography)": [
+		'Abdomen',
+		'Chest(Excluding Myocardium)',
+		'Head',
+		'Lower Extremity',
+		'Neck',
+		'Pelvis',
+		'Upper extremity',
+	],
+
+	"MR Arthrogram": [
+		'Hip, Knee, or Ankle, (Lower Extremity)',
+		'Shoulder, Elbow or Wrist(Upper Extremity)',
+	],
+	
+	"Non-MR Arthrogram": [
+		'Ankle',
+		'Elbow',
+		'Hip',
+		'Knee',
+		'Shoulder',
+	]
+};
+
---
+++ services.js
@@ -0,0 +1,8 @@
+const services = [
+    'CT Scan',
+    'MRI',
+    'MRA-MRV(Angiography)',
+    'MR Arthrogram',
+    'Non-MR Arthrogram'
+];
+
